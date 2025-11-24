05278187b546bdd03b46fe6a857a111ce7312879
Mohamed-ds-au <111690029+Mohamed-ds-au@users.noreply.github.com>
2025-11-24 14:52:33 +0200

    insert from csv

--- SQL/Insert.sql
+++ SQL/Insert.sql
@@ -1,5 +1,6 @@
 -- 7. DUMMY DATA (For Testing)
 -- =======================================================================================
+USE library_db;
 
 -- Insert Admin Login
 INSERT INTO Login (Username, Login_Password, Entity_Type) VALUES ('admin', 'admin123', 'Admin');
@@ -13,3 +14,11 @@ INSERT INTO Books (Title, Author, Quantity, Available_Copies, Price, Category) V
 
 -- Register a Customer via Procedure
 CALL RegisterMember('john_doe', 'pass123', 'John', 'Doe', 'john@example.com');
+
+LOAD DATA INFILE 'Path-to-CSV'
+INTO TABLE books
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS
+(Category, Title, Author, Price, Quantity, Available_Copies, Pub_Year, Pub_Name, Cover, Rating, Availability);
